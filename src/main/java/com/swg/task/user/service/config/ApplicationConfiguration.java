97442cddd3d2ce6316413aa9180dd9f978c999a2
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2026-02-23 14:25:22 +0530

    feat : cors config and BCrypt pass enc

--- src/main/java/com/swg/task/user/service/config/ApplicationConfiguration.java
+++ src/main/java/com/swg/task/user/service/config/ApplicationConfiguration.java
@@ -3,18 +3,26 @@ package com.swg.task.user.service.config;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.jspecify.annotations.Nullable;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
 @Configuration
 public class ApplicationConfiguration {
 
+    @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http.sessionManagement(
 
@@ -23,7 +31,7 @@ public class ApplicationConfiguration {
                 )
         ).authorizeHttpRequests(
                 Authorize-> Authorize.requestMatchers("/api/**").authenticated().anyRequest().permitAll()
-        ).addFilterBefore(null, BasicAuthenticationFilter.class)
+        ).addFilterBefore(new JwtTokenValidator, BasicAuthenticationFilter.class)
                         .csrf(csrf->csrf.disable()).cors(cors->cors
                         .configurationSource(corsConfigurationSource() ))
                         .httpBasic(Customizer.withDefaults())
@@ -37,8 +45,23 @@ public class ApplicationConfiguration {
         return new CorsConfigurationSource() {
             @Override
             public @Nullable CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-                return null;
+
+                CorsConfiguration cfg = new CorsConfiguration();
+                cfg.setAllowedOrigins(Collections.singletonList("*"));
+                cfg.setAllowedMethods(Collections.singletonList("*"));
+                cfg.setAllowCredentials(true);
+                cfg.setAllowedHeaders(Collections.singletonList("*"));
+                cfg.setExposedHeaders(Arrays.asList("Authorization"));
+                cfg.setMaxAge(3600L);
+
+                return cfg;
             }
         };
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
