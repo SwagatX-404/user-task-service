3dcaefc310d5893c9eeebd7ff1857e27c71b03a2
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2026-02-27 20:55:06 +0530

    ref: /

--- src/main/java/com/swg/task/user/service/config/JwtProvider.java
+++ src/main/java/com/swg/task/user/service/config/JwtProvider.java
@@ -3,7 +3,6 @@ package com.swg.task.user.service.config;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-
 import javax.crypto.SecretKey;
 import java.util.Collection;
 
@@ -14,13 +13,11 @@ public class JwtProvider {
     public static String generateToken(Authentication auth) {
 
         Collection<? extends GrantedAuthority> authorities = auth.getAuthorities();
-
         String roles = populateAuthorities(authorities);
             return null;
     }
 
     private static String populateAuthorities(Collection<? extends GrantedAuthority> collection) {
-
         return null;
     }
 }
