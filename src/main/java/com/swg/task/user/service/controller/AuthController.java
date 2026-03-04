e74a9de4d947861acf62584981f816f2339a379d
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2026-03-04 21:20:32 +0530

    ref:

--- src/main/java/com/swg/task/user/service/controller/AuthController.java
+++ src/main/java/com/swg/task/user/service/controller/AuthController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
@@ -33,7 +32,6 @@ public class AuthController {
     @Autowired
     private CustomerUserServiceImplementation customerUserDetails;
 
-
     @PostMapping("/signup")
     public ResponseEntity<AuthResponse> createUserHandler(
             @RequestBody User user) throws Exception {
@@ -41,38 +39,35 @@ public class AuthController {
         String email = user.getEmail();
         String password = user.getPassword();
         String fullName = user.getFullName();
-        String role=user.getRole();
+        String role = user.getRole();
 
         User isEmailExist = userRepository.findByEmail(email);
 
         if (isEmailExist != null) {
-    throw new Exception("Email Is Already Used With Another Account");
+            throw new Exception("Email Is Already Used With Another Account");
         }
 
-    // Create new user
-         User createdUser = new User();
+        // Create new user
+        User createdUser = new User();
         createdUser.setEmail(email);
         createdUser.setFullName(fullName);
         createdUser.setRole(role);
         createdUser.setPassword(passwordEncoder.encode(password));
 
-             User savedUser = userRepository.save(createdUser);
+        User savedUser = userRepository.save(createdUser);
 
-         Authentication authentication = new UsernamePasswordAuthenticationToken(email, password);
-         SecurityContextHolder.getContext().setAuthentication(authentication);
+        Authentication authentication = new UsernamePasswordAuthenticationToken(email, password);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
 
-         String token = JwtProvider.generateToken(authentication);
+        String token = JwtProvider.generateToken(authentication);
 
         AuthResponse authResponse = new AuthResponse();
         authResponse.setJwt(token);
         authResponse.setMessage("Register Success");
         authResponse.setStatus(true);
 
-            return new ResponseEntity<>(authResponse,HttpStatus.OK);
+        return new ResponseEntity<>(authResponse, HttpStatus.OK);
     }
-
-
-
 
     @PostMapping("/signin")
     public ResponseEntity<AuthResponse> signin(@RequestBody LoginRequest loginRequest) {
@@ -101,17 +96,15 @@ public class AuthController {
 
         System.out.println(("sign in userDetails - " + userDetails));
 
-
         if (userDetails == null) {
             System.out.println("sign in userDetails - null " + userDetails);
             throw new BadCredentialsException("Invalid username or password");
         }
-            if (!passwordEncoder.matches(password, userDetails.getPassword())) {
-                System.out.println("sign in userDetails - password not match " + userDetails);
-                throw new BadCredentialsException("Invalid username or password");
+        if (!passwordEncoder.matches(password, userDetails.getPassword())) {
+            System.out.println("sign in userDetails - password not match " + userDetails);
+            throw new BadCredentialsException("Invalid username or password");
 
-
-             }
-            return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
         }
+        return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
     }
+}
